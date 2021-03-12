c67cd95348b977bd9b6ad8a6e363900a63a1cb65
Ed Davis <ed.davis@bjss.com>
2021-03-12 16:58:31 +0000

    Adding mockend mock api

--- basic_API_test_spec.rb
+++ basic_API_test_spec.rb
@@ -5,5 +5,13 @@ describe http('https://jsonplaceholder.typicode.com/todos/1') do
   its('body') { should include '"title": "delectus aut autem"'}
   its('body') { should include '"completed": false'}
   its('body') { should include '"id": 1,'}
+  its('status') { should cmp 200 }
+end
+
+describe http('GET https://mockend.com/Ed-Davis/basic_inspec_API_test_example/posts') do
+  its('body') { should include '"title": "Testing APIs is cool"'}
+  its('body') { should include '"views": "All my own"'}
+  its('body') { should include '"createdAt": "12-03-21"'}
+  its('body') { should include '"body": "InSpec can be fun!"'}
   its('status') { should cmp 301 }
 end
