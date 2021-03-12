e08c194dbeb4ae14b43d7d9eae8a7d5fbee7f1bb
Ed Davis <ed.davis@bjss.com>
2021-03-12 17:50:03 +0000

    Removed mockend

--- basic_API_test_spec.rb
+++ basic_API_test_spec.rb
@@ -7,11 +7,3 @@ describe http('https://jsonplaceholder.typicode.com/todos/1') do
   its('body') { should include '"id": 1,'}
   its('status') { should cmp 200 }
 end
-
-describe http('GET https://mockend.com/Ed-Davis/basic_inspec_API_test_exampleposts?date_order=desc') do
-  its('body') { should include '"title": "Testing APIs is cool"'}
-  its('body') { should include '"views": "All my own"'}
-  its('body') { should include '"createdAt": "12-03-21"'}
-  its('body') { should include '"body": "InSpec can be fun!"'}
-  its('status') { should cmp 301 }
-end
